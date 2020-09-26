602ba6ec649df9bdd4cc7f93ed7dfb9382d65217
pras-enunui <54830426+pras-enunui@users.noreply.github.com>
2020-09-26 08:46:19 +0530

    commit by prasanth

--- app/src/main/java/com/example/mybasic/MainActivity.java
+++ app/src/main/java/com/example/mybasic/MainActivity.java
@@ -2,79 +2,93 @@ package com.example.mybasic;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MultipleChoiceDialogFragment.onMultiChoiceListener {
+public class MainActivity extends AppCompatActivity  {
 
-    SearchableSpinner searchableSpinner;
-    ArrayList<String>arrayList = new ArrayList<>();
-    TextView tvSelectedChoices;
+    private RecyclerView mRecyclerView;
+    private ArrayList<ExampleItem> mExampleList;
+    private ExampleAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Button buttonInsert;
+    private Button buttonRemove;
+    private EditText editTextInsert;
+    private EditText editTextRemove;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvSelectedChoices = findViewById(R.id.test);
-
-        Button btnSelectChoices = findViewById(R.id.btnSelectChoices);
-        btnSelectChoices.setOnClickListener(new View.OnClickListener() {
+        createExampleList();
+        buildRecyclerView();
+        setButtons();
+    }
+    public void insertItem(int position) {
+        mExampleList.add(position, new ExampleItem(R.drawable.ic_baseline_sports_handball, "New Item At Position" + position, "This is Line 2"));
+        mAdapter.notifyItemInserted(position);
+    }
+    public void removeItem(int position) {
+        mExampleList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+    }
+    public void changeItem(int position, String text) {
+        mExampleList.get(position).changeText1(text);
+        mAdapter.notifyItemChanged(position);
+    }
+    public void createExampleList() {
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.ic_baseline_sports_handball, "Line 1", "Line 2"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_baseline_sports_handball, "Line 3", "Line 4"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_baseline_sports_handball, "Line 5", "Line 6"));
+    }
+    public void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(new ExampleAdapter.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                DialogFragment multiChoiceDialog = new MultipleChoiceDialogFragment();
-                multiChoiceDialog.setCancelable(false);
-                multiChoiceDialog.show(getSupportFragmentManager(), "Multichoice Dialog");
+            public void onItemClick(int position) {
+                changeItem(position, "Clicked");
+            }
+            @Override
+            public void onDeleteClick(int position) {
+                removeItem(position);
             }
         });
-        /*searchableSpinner = findViewById(R.id.spiner);
-
-        arrayList.add("thala");
-        arrayList.add("thalapathy");
-        arrayList.add("vijay");
-        arrayList.add("prasanth");
-        arrayList.add("aijth");
-        arrayList.add("mari");
-        arrayList.add("muthu");
-        arrayList.add("thala");
-        arrayList.add("thalapathy");
-        arrayList.add("vijay");
-        arrayList.add("prasanth");
-        arrayList.add("aijth");
-        arrayList.add("mari");
-        arrayList.add("muthu");
-        arrayList.add("thala");
-        arrayList.add("thalapathy");
-        arrayList.add("vijay");
-        arrayList.add("prasanth");
-        arrayList.add("aijth");
-        arrayList.add("mari");
-        arrayList.add("muthu");
-
-        ArrayAdapter<String> adapter =  new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,arrayList);
-        searchableSpinner.setAdapter(adapter);*/
     }
-    @Override
-    public void onPositiveButtonClicked(String[] list, ArrayList<String> selectedItemList) {
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Selected Choices = ");
-        for (String str : selectedItemList) {
-            stringBuilder.append(str + ",");
-        }
-        tvSelectedChoices.setText(stringBuilder);
-
-    }
-
-    @Override
-    public void onNegativeButtonClicked() {
-        tvSelectedChoices.setText("Dialog Cancel");
+    public void setButtons() {
+        buttonInsert = findViewById(R.id.button_insert);
+        buttonRemove = findViewById(R.id.button_remove);
+        editTextInsert = findViewById(R.id.edittext_insert);
+        editTextRemove = findViewById(R.id.edittext_remove);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
     }
 }
